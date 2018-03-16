153bc8e7ba3f31d465dcadd6c0dc4fa84bd75f7f
leveno <857596898@qq.com>
2018-03-16 11:29:03 +0800

    20180316

---
+++ public/javascripts/banner.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    // $('.banner .pointer li').hover(function(){
+    //     var self = $(this)
+    //     var index = self.data('index')
+    //
+    //     self.parent('ul.pointer').siblings('img').eq(index).addClass('active').siblings('img').removeClass('active')
+    //     self.find('span').addClass('active').parent('li').siblings('li').find('span').removeClass('active')
+    // })
+    //
+    // setInterval(function(){
+    //     var self = $('.contain li span.active ')
+    //     var index = self.parent('li').data('index')
+    //     var length = self.parent('ul.pointer').find('li').length
+    //     self.parents('ul.pointer').siblings('img').eq(index).removeClass('active')
+    //     self.removeClass('active').parents('ul.pointer').find('li')
+    //     if (index < length - 1) {
+    //         $('.contain img').eq(index + 1).addClass('active')
+    //         $('.contain li').eq(index + 1).find('span').addClass('active')
+    //     }else {
+    //         $('.contain img').eq(0).addClass('active')
+    //         $('.contain li').eq(0).find('span').addClass('active')
+    //     }
+    // },3000)
+    var mySwiper = new Swiper ('.swiper-container', {
+        //direction: 'vertical',
+        loop: true,
+        autoplay:true,
+
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // 如果需要前进后退按钮
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // 如果需要滚动条
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    })
+})
